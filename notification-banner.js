1d8f398eafea17a5e5d9469b837469ccec99fa42
Biswa <biswaranjan0009@gmail.com>
2018-12-02 18:09:55 +0530

    updated style and usage

--- notification-banner.js
+++ notification-banner.js
@@ -1,5 +1,5 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
+import '@polymer/paper-dialog/paper-dialog.js';
 /**
  * `notification-banner`
  * a customization notification banner in Polymer 3.0
@@ -14,19 +14,77 @@ class NotificationBanner extends PolymerElement {
       <style>
         :host {
           display: block;
+          box-sizing: border-box;
         }
+        paper-dialog {
+          width: 100%;
+          height: var(--notification-banner-height, 76px);
+          position: relative;
+          border-radius: 5px;
+          padding-top: 25px;
+          padding-left: 40%;
+        }
+
+        paper-dialog.success {
+          border: 1px solid #4caf50;
+          background-color: var(--notification-banner-bg-color, #f1f8e9);
+          color: var(--notification-banner-color, #4caf50);
+        }
+
+        paper-dialog.error {
+          border: 1px solid #f50808;
+          background-color: var(--notification-banner-bg-color, #f7d7d7);
+          color: var(--notification-banner-color, #f50808);
+        }
+
+        paper-dialog.warning {
+          border: 1px solid #4caf50;
+          background-color: var(--notification-banner-bg-color, #f1f8e9);
+          color: var(--notification-banner-color, #4caf50);
+        }
+
       </style>
-      <h2>Hello [[prop1]]!</h2>
+      <paper-dialog opened class$="[[className]]" modal>
+        [[text]]
+      </paper-dialog>
     `;
   }
+
+  static get is() {
+    return 'notification-banner';
+  }
+
   static get properties() {
     return {
-      prop1: {
+      version: {
         type: String,
-        value: 'notification-banner',
+        value: ""
       },
+      isVisible: {
+        type: Boolean,
+        readonly: true,
+        value: false
+      },
+      text: {
+        type: String,
+        value: ""
+      },
+      type: {
+        type: String,
+        value: "",
+        observer: '_computeClass'
+      }
     };
+  }
+
+  _computeClass(newVal) {
+    if(newVal === 'success') {
+      this.className= "success";
+    }
+    if(newVal === 'error') {
+      this.className= "error";
+    }
   }
 }
 
-window.customElements.define('notification-banner', NotificationBanner);
+window.customElements.define(NotificationBanner.is, NotificationBanner);
